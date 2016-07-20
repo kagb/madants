8b87d289c60af7790274d1cc9ebd147b4fcc2e75
kagb <ka@evilka.com>
2016-07-20 12:25:57 +0800

    use peewee as orm

--- models/tables.py
+++ models/tables.py
@@ -1,30 +1,29 @@
 # -*- coding: utf-8 -*-
 
-import arrow
-from sqlalchemy import Column, DateTime, Integer, String
-from sqlalchemy.ext.declarative import declarative_base
-
-Base = declarative_base()
+from peewee import ForeignKeyField, CharField
+from .base import BaseModel
 
 
-class Model(Base):
-    id = Column(Integer, primary_key=True)
-    create_time = Column(DateTime, default=arrow.now().naive)
-
-
-class Ant(Model):
-
-    domain = 'Ant'
-
-    name = Column(String)
-    email = Column(String)
-    passwd = Column(String)
-    website_id = Column(Integer)
-    refer_id = Column(Integer)
-
-
-class WebSite(Model):
+class WebSite(BaseModel):
 
     domain = 'WebSite'
 
-    name = Column(String)
+    name = CharField()
+
+
+class Army(BaseModel):
+
+    domain = 'Army'
+
+    name = CharField()
+    website = ForeignKeyField(WebSite, related_name='armies')
+
+
+class Ant(BaseModel):
+
+    domain = 'Ant'
+
+    name = CharField()
+    email = CharField()
+    passwd = CharField()
+    army = ForeignKeyField(Army, related_name='ants')
