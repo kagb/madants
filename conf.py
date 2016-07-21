a166547f35437d58255a4143fb8a11e8c159a5d0
kagb <ka@evilka.com>
2016-07-21 17:09:44 +0800

    add zhihu login

--- conf.py
+++ conf.py
@@ -7,6 +7,12 @@ MYSQL_USER = 'madqueen'
 MYSQL_PASSWD = ''
 
 
+PC_USER_AGENT = 'Mozilla/5.0 (Windows NT 5.1; rv:33.0) Gecko/20100101 Firefox/33.0'
+PC_HEADERS = {
+    'User-Agent': PC_USER_AGENT,
+}
+
+
 try:
     from local_conf import *
 except ImportError:
